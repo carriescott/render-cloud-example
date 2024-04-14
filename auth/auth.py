dae528a464e536f62cc7b0c1bc1aa78e16a3ef27
Carrie <carriescott0@gmail.com>
2024-04-14 17:02:33 +0100

    environment configs

--- auth/auth.py
+++ auth/auth.py
@@ -5,12 +5,12 @@ from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
-# AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
-AUTH0_DOMAIN ='carrie-capstone-agency.uk.auth0.com'
-# ALGORITHMS = os.environ.get['ALGORITHMS']
-ALGORITHMS = ['RS256']
-# API_AUDIENCE = os.environ.get('API_AUDIENCE')
-API_AUDIENCE = 'https://capstone-agency/'
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+# AUTH0_DOMAIN ='carrie-capstone-agency.uk.auth0.com'
+ALGORITHMS = os.environ.get['ALGORITHMS']
+# ALGORITHMS = ['RS256']
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
+# API_AUDIENCE = 'https://capstone-agency/'
 
 class AuthError(Exception):
     def __init__(self, error, status_code):
